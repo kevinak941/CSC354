641069d55f3eaee48c70f4cc0839c1b70b297b50
Kevin <kevin@agileinterfaces.com>
2014-03-14 18:26:07 -0400

    Additional tag models

---
+++ application/models/tag_group_tags_m.php
@@ -0,0 +1,15 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Used to store tag group data
+ *
+ * @author Kevin Kern
+ * @version 1.0
+ */
+class Tag_groups_m extends MY_Model {
+	public $_table = 'tag_groups';
+	public $primary_key = 'id';
+}
+
+/* End of file tag_groups_m.php */
+/* Location: ./application/models/tag_groups_m.php */
---
+++ application/models/tag_groups_m.php
@@ -0,0 +1,15 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Used to store tag group data
+ *
+ * @author Kevin Kern
+ * @version 1.0
+ */
+class Tag_group_tags_m extends MY_Model {
+	public $_table = 'tag_group_tags';
+	public $primary_key = 'id';
+}
+
+/* End of file tag_group_tags_m.php */
+/* Location: ./application/models/tag_group_tags_m.php */
