89f56ee8027ace0fc538c16ed0182a3b569f08d5
psche424 <psche424@live.kutztown.edu>
2014-04-14 15:39:38 -0400

    test commit

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -24,44 +24,6 @@
 	}
 </script>
 <div data-role="page" id="p_dashboard" ng-controller="p_dashboard">
-	<div data-role="header" class="ui-persist">
-		<a data-icon="gear" class="ui-btn-left" onclick="show_menu(this);">Menu</a>
-		<h1>Dashboard</h1>
-		<a data-icon="gear" class="ui-btn-right">Options</a>
-	</div>
-	<div data-role="content">
-	<center>	
-	<div class="ui-grid-b">
-		<div class="ui-block-a">Left Picture</div>
-		<div class="ui-block-b">John Doe</div>
-		<div class="ui-block-c">Right Picture</div>
-	</div><!-- /grid-b -->
-	</center><script type="text/javascript">
-	function p_dashboard($scope, selectedService) {
-		$scope.feed = [];
-
-		$scope.get_feed	=	function() {
-			jQuery.post("pages/dashboard", {}, function(data) {
-				if(data.status == "success") {
-					jQuery.each(data.data, function(i, item) {
-						$scope.feed[i] = item;
-					});
-					$scope.$apply();
-				}
-			}, "json");
-		};
-
-		/**
-		 * Triggers a detail view for a specific object
-		 */
-		$scope.view		=	function(id) {
-			selectedService.id = id;
-		}
-
-		$scope.get_feed();
-	}
-</script>
-<div data-role="page" id="p_dashboard" ng-controller="p_dashboard">
 	<div data-role="header">
 		<h1>Dashboard</h1>
 		<a data-icon="gear" class="ui-btn-right">Options</a>
