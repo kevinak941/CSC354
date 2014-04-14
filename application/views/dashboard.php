93155126428da49ae1c88b3598f464ce345bb5f9
psche424 <psche424@live.kutztown.edu>
2014-04-14 15:35:49 -0400

    View Edit Example commit

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -29,12 +29,59 @@
 		<a data-icon="gear" class="ui-btn-right">Options</a>
 	</div>
 	<div data-role="content">
-		<h2>Dashboard</h2>
+	<center>	
+	<div class="ui-grid-b">
+		<div class="ui-block-a">Left Picture</div>
+		<div class="ui-block-b">John Doe</div>
+		<div class="ui-block-c">Right Picture</div>
+	</div><!-- /grid-b -->
+	</center><script type="text/javascript">
+	function p_dashboard($scope, selectedService) {
+		$scope.feed = [];
+
+		$scope.get_feed	=	function() {
+			jQuery.post("pages/dashboard", {}, function(data) {
+				if(data.status == "success") {
+					jQuery.each(data.data, function(i, item) {
+						$scope.feed[i] = item;
+					});
+					$scope.$apply();
+				}
+			}, "json");
+		};
+
+		/**
+		 * Triggers a detail view for a specific object
+		 */
+		$scope.view		=	function(id) {
+			selectedService.id = id;
+		}
+
+		$scope.get_feed();
+	}
+</script>
+<div data-role="page" id="p_dashboard" ng-controller="p_dashboard">
+	<div data-role="header">
+		<h1>Dashboard</h1>
+		<a data-icon="gear" class="ui-btn-right">Options</a>
+	</div>
+	<div data-role="content">
 		
+	<div class="ui-grid-b"><!-- Top Row -->
+		<div class="ui-block-a">Left Picture</div>
+		<div class="ui-block-b">John Doe</div> 
+		<div class="ui-block-c">Right Picture</div>
+	</div>
+	
+	<div class="ui-grid-d">
+		<div class="ui-block-a">00</div>
+		<div class="ui-block-b">00</div>
+		<div class="ui-block-c">00</div>
+		<div class="ui-block-d">00</div>
+		<div class="ui-block-e">00</div>
+	</div>		
 		
-		
-		
-		
+	
 		
 		
 	</div><!-- CLose content -->
