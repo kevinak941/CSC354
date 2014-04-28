a4af6a3dfb412fdfac99f82231741a9161988bed
Kevin <kevin@agileinterfaces.com>
2014-04-28 13:40:22 -0400

    Modifications for banner

--- application/views/dashboard_header.php
+++ application/views/dashboard_header.php
@@ -1,4 +1,4 @@
 <div data-role="header" class="ui-persist" data-position="fixed" data-tap-toggle="false">
 	<a data-icon="grid" data-iconpos="notext" onclick="show_menu(this);">Menu</a>
-	<h1>BudgetChef</h1>
+	<h1>Budget Chef</h1>
 </div>
